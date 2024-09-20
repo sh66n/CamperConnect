459c596a6008332b096cd80fafb30e5df5539be2
sh66n <shaanedroos85@gmail.com>
2024-09-20 20:13:19 +0530

    init repo

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./views/Landing";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
