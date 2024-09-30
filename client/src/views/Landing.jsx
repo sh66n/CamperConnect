0ca0904412c33745d2f136d684243ea6007afa68
sh66n <shaanedroos85@gmail.com>
2024-09-30 23:13:12 +0530

    fixed landing page styling

--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -6,21 +6,24 @@ export default function Landing() {
   return (
     <>
       <div className="min-h-screen h-auto w-100 bg-black text-white flex flex-col">
-        <div className="text-red-500 text-center">
-          <Typewriter
-            options={{
-              strings: ["Minimalist", "Trek", "Roam", "Wander"],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-        </div>
         <div className="w-full grow flex justify-center items-center">
           <div className="h-96 aspect-square bg-red-100 bg-[url('https://res.cloudinary.com/dkhlgn6zs/image/upload/v1727637012/What_you_need_to_know_before_investing_in_a_VW_camper_van__Katrina_Stewart_Photography_sjxe3t.jpg')] bg-cover rounded-xl mr-8"></div>
           <div className="w-1/4">
-            <h1 className=" font-bold text-3xl text-center mb-4">
-              Camper<span className="text-red-500">Connect</span>
-            </h1>
+            <div className="mb-4">
+              <h1 className="font-bold text-3xl text-center">
+                Camper<span className="text-red-500">Connect</span>
+              </h1>
+              <div className="text-red-500 text-center">
+                <Typewriter
+                  options={{
+                    strings: ["Minimalist", "Trek", "Roam", "Wander"],
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </div>
+            </div>
+
             <p className="mb-2">
               A minimalist platform linking campers to outdoor destinations for
               easy, seamless adventures.
